b7baea1bff3872007cf476adbbec1e91f3ddd9cc
ITxiaohao <shunhaozeng@gmail.com>
2019-03-15 16:27:55 +0800

    :sparkles: 处理第三方 js 库

---
+++ demo13/src/app.js
@@ -0,0 +1,7 @@
+// app.js
+$('div').addClass('new')
+
+jQuery('div').addClass('old')
+
+// 运行webpack后
+// 浏览器打开 index.html, 查看 div 标签的 class
---
+++ demo13/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    publicPath: './', // js 引用的路径或者 CDN 地址
+    path: path.resolve(__dirname, 'dist'), // 打包文件的输出目录
+    filename: '[name].bundle.js', // 代码打包后的文件名
+    chunkFilename: '[name].js' // 代码拆分后的文件名
+  },
+  resolve: {
+    alias: {
+      jQuery$: path.resolve(__dirname, 'src/vendor/jquery.min.js')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      // 打包输出HTML
+      title: '自动生成 HTML',
+      minify: {
+        // 压缩 HTML 文件
+        removeComments: true, // 移除 HTML 中的注释
+        collapseWhitespace: true, // 删除空白符与换行符
+        minifyCSS: true // 压缩内联 css
+      },
+      filename: 'index.html', // 生成后的文件名
+      template: 'index.html', // 根据此模版生成 HTML 文件
+      chunks: ['app'] // entry中的 app 入口才会被打包
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery', // npm
+      jQuery: 'jQuery' // 本地Js文件
+    })
+  ]
+}
