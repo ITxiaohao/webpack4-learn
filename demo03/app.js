60f07ac9875c1b0a6fd79a3b3470f4ae8bc3ed20
ITxiaohao <shunhaozeng@gmail.com>
2019-03-15 16:13:18 +0800

    :sparkles: 覆盖默认 entry/output

---
+++ demo03/app.js
@@ -0,0 +1,16 @@
+/**
+ * webpack 支持 ES6、CommonJs 和 AMD 规范
+ */
+
+// ES6
+import sum from './vendor/sum'
+console.log('sum(1, 2) = ', sum(1, 2))
+
+// CommonJs
+var minus = require('./vendor/minus')
+console.log('minus(1, 2) = ', minus(1, 2))
+
+// // AMD
+// require(['./vendor/multi'], function(multi) {
+//   console.log('multi(1, 2) = ', multi(1, 2))
+// })
---
+++ demo03/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './app.js' // 需要打包的文件入口
+  },
+  output: {
+    publicPath: __dirname + '/dist/', // js 引用的路径或者 CDN 地址
+    path: path.resolve(__dirname, 'dist'), // 打包文件的输出目录
+    filename: 'bundle.js' // 打包后生产的 js 文件
+  },
+  plugins: [
+    new CleanWebpackPlugin() // 默认情况下，此插件将删除 webpack output.path目录中的所有文件，以及每次成功重建后所有未使用的 webpack 资产。
+  ]
+}
