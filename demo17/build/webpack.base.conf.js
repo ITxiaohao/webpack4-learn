1597ce28482f2e269c1ddb17b30da8aa500a7465
ITxiaohao <shunhaozeng@gmail.com>
2019-03-21 22:33:06 +0800

    :sparkles: 配置 PWA

---
+++ demo17/build/webpack.base.conf.js
@@ -0,0 +1,79 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '..', 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name]-[hash:5].min.[ext]',
+              limit: 1000, // size <= 1KB
+              outputPath: 'images/'
+            }
+          },
+          // img-loader for zip img
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              // 压缩 jpg/jpeg 图片
+              mozjpeg: {
+                progressive: true,
+                quality: 65 // 压缩率
+              },
+              // 压缩 png 图片
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|ttf|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name]-[hash:5].min.[ext]',
+            limit: 5000, // fonts file size <= 5KB, use 'base64'; else, output svg file
+            publicPath: 'fonts/',
+            outputPath: 'fonts/'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    // 开发环境和生产环境二者均需要的插件
+    new HtmlWebpackPlugin({
+      title: 'webpack4 实战',
+      filename: 'index.html',
+      template: path.resolve(__dirname, '..', 'index.html'),
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    new webpack.ProvidePlugin({ $: 'jquery' })
+  ],
+  performance: false
+}
---
+++ demo17/build/webpack.dev.conf.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const commonConfig = require('./webpack.base.conf.js')
+
+const path = require('path')
+
+const devConfig = {
+  mode: 'development',
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2 // 在一个 css 中引入了另一个 css，也会执行之前两个 loader，即 postcss-loader 和 sass-loader
+            }
+          },
+          'sass-loader', // 使用 sass-loader 将 scss 转为 css
+          'postcss-loader' // 使用 postcss 为 css 加上浏览器前缀
+        ]
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-soure-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../dist/'),
+    port: 8000,
+    hot: true,
+    overlay: true,
+    proxy: {
+      '/comments': {
+        target: 'https://m.weibo.cn',
+        changeOrigin: true,
+        logLevel: 'debug',
+        headers: {
+          Cookie: ''
+        }
+      }
+    },
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+}
+
+module.exports = merge(commonConfig, devConfig)
