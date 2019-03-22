d580981e4ca9a8eb693ed18827e06aee04142d49
ITxiaohao <shunhaozeng@gmail.com>
2019-03-22 17:26:36 +0800

    :sparkles: 使用 DllPlugin 加快打包速度

---
+++ demo20/build/webpack.base.conf.js
@@ -0,0 +1,103 @@
+const path = require('path')
+const fs = require('fs')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const AddAssetHtmlWebpackPlugin = require('add-asset-html-webpack-plugin')
+
+// 存放公共插件
+const plugins = [
+  // 开发环境和生产环境二者均需要的插件
+  new HtmlWebpackPlugin({
+    title: 'webpack4 实战',
+    filename: 'index.html',
+    template: path.resolve(__dirname, '..', 'index.html'),
+    minify: {
+      collapseWhitespace: true
+    }
+  }),
+  new webpack.ProvidePlugin({ $: 'jquery' })
+]
+
+// 自动引入 dll 中的文件
+const files = fs.readdirSync(path.resolve(__dirname, '../dll'))
+files.forEach(file => {
+  if (/.*\.dll.js/.test(file)) {
+    plugins.push(
+      new AddAssetHtmlWebpackPlugin({
+        filepath: path.resolve(__dirname, '../dll', file)
+      })
+    )
+  }
+  if (/.*\.manifest.json/.test(file)) {
+    plugins.push(
+      new webpack.DllReferencePlugin({
+        manifest: path.resolve(__dirname, '../dll', file)
+      })
+    )
+  }
+})
+
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '..', 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name]-[hash:5].min.[ext]',
+              limit: 1000, // size <= 1KB
+              outputPath: 'images/'
+            }
+          },
+          // img-loader for zip img
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              // 压缩 jpg/jpeg 图片
+              mozjpeg: {
+                progressive: true,
+                quality: 65 // 压缩率
+              },
+              // 压缩 png 图片
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|ttf|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name]-[hash:5].min.[ext]',
+            limit: 5000, // fonts file size <= 5KB, use 'base64'; else, output svg file
+            publicPath: 'fonts/',
+            outputPath: 'fonts/'
+          }
+        }
+      }
+    ]
+  },
+  plugins,
+  performance: false
+}
