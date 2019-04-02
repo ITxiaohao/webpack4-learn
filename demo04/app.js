c05dc7f70e15999df2f4eb437555845e8187ef79
ITxiaohao <shunhaozeng@gmail.com>
2019-04-02 16:34:45 +0800

    :sprkles: 使用 useBuiltIns 只转译使用的 ES6 语法

--- demo04/app.js
+++ demo04/app.js
@@ -1,8 +1,14 @@
 // 全局引入
-import '@babel/polyfill'
+// import '@babel/polyfill'
 
 // 测试 ES6 语法是否通过 babel 转译
 const array = [1, 2, 3]
 const isES6 = () => console.log(...array)
 
 isES6()
+
+const arr = [new Promise(() => {}), new Promise(() => {})]
+
+arr.map(item => {
+  console.log(item)
+})
