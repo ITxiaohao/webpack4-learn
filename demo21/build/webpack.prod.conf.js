c7fd288ad04709943a30793eba8a5f02c4261655
ITxiaohao <shunhaozeng@gmail.com>
2019-05-14 09:36:56 +0800

    :art: 调整 postcss-loader 的执行顺序

--- demo21/build/webpack.prod.conf.js
+++ demo21/build/webpack.prod.conf.js
@@ -25,8 +25,8 @@ const prodConfig = {
               importLoaders: 2 // 在一个 css 中引入了另一个 css，也会执行之前两个 loader，即 postcss-loader 和 sass-loader
             }
           },
-          'sass-loader', // 使用 sass-loader 将 scss 转为 css
-          'postcss-loader' // 使用 postcss 为 css 加上浏览器前缀
+          'postcss-loader', // 使用 postcss 为 css 加上浏览器前缀
+          'sass-loader' // 使用 sass-loader 将 scss 转为 css
         ]
       }
     ]
